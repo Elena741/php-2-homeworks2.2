4a1d55bbf541cb9a39be6531359661b76667962a
Elena741 <solo741@yandex.ru>
2018-10-22 18:58:55 +0400

    Стартовая страница

---
+++ admin.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+     <style>
+        div {
+            width: 400px;
+            border: 1px solid black;
+            padding: 30px;
+        }
+    </style>
+    <title>Загрузить тест</title>
+</head>
+<body>
+
+    <div>
+        <p>Загрузите ваш тест - файл с расширением JSON</p>
+
+<?php
+if (isset($_POST) && isset($_FILES) && isset($_FILES['testfile'])) {
+    $fileName = $_FILES['testfile']['name'];
+    $tmpFile = $_FILES['testfile']['tmp_name'];
+    $uploadsDir = 'uploads/';
+
+    $pathInfo = pathinfo($uploadsDir . $fileName);
+
+    if ($pathInfo['extension'] === 'json') {
+        move_uploaded_file($tmpFile, $uploadsDir . $fileName);
+        ?>
+        <p>Спасибо, Ваш тест загружен!</p>
+        <?php
+    } else {
+        ?>
+        <p>Извините, нужен файл с расширением JSON</p>
+        <?php
+    }        
+}   
+?>
+        <form method="post" enctype="multipart/form-data">
+            <input type="file" name="testfile">
+            <input type="submit" value="Загрузить">
+        </form>
+
+        <p>Выберите тест для прохождения</p>
+        <ul>
+            <li><a href="list.php">Список тестов</a></li>
+        </ul>
+    </div>
+
+</body>
+</html>
